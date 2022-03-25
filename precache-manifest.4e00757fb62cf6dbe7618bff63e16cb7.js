4b9845bdd4ea5c183e956721616de182c39a599d
Wallace Nascimento <87508400+wallace-nascimento@users.noreply.github.com>
2022-03-25 02:22:11 -0300

    Updates

---
+++ precache-manifest.4e00757fb62cf6dbe7618bff63e16cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46a368bce1cffdc70e92",
+    "url": "/teste/static/css/main.25a8e49a.chunk.css"
+  },
+  {
+    "revision": "46a368bce1cffdc70e92",
+    "url": "/teste/static/js/main.46a368bc.chunk.js"
+  },
+  {
+    "revision": "bae3b8b2219336c77219",
+    "url": "/teste/static/css/1.e9f4d01d.chunk.css"
+  },
+  {
+    "revision": "bae3b8b2219336c77219",
+    "url": "/teste/static/js/1.bae3b8b2.chunk.js"
+  },
+  {
+    "revision": "b0f3b10a8f55a4e23d98",
+    "url": "/teste/static/js/runtime~main.b0f3b10a.js"
+  },
+  {
+    "revision": "20ba55c2aed1f6c7e966c47febb20901",
+    "url": "/teste/index.html"
+  }
+];
